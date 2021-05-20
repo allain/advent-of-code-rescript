56419f18b3bac527d6ef80025655b252b9dcb1db
allain <allain.lalonde@gmail.com>
2021-05-20 12:42:15 -0400

    day 5 part 2

--- 2020/day5/src/Run.mjs
+++ 2020/day5/src/Run.mjs
@@ -3,11 +3,14 @@
 import * as Fs from "fs";
 import * as Js_math from "rescript/lib/es6/js_math.js";
 import * as Belt_Array from "rescript/lib/es6/belt_Array.js";
+import * as Belt_SetInt from "rescript/lib/es6/belt_SetInt.js";
 import * as Caml_exceptions from "rescript/lib/es6/caml_exceptions.js";
 
-var lines = Fs.readFileSync("input.txt", "utf-8").split("\n");
-
 var InvalidLetter = /* @__PURE__ */Caml_exceptions.create("Run.InvalidLetter");
+
+var NoEmptySeat = /* @__PURE__ */Caml_exceptions.create("Run.NoEmptySeat");
+
+var lines = Fs.readFileSync("input.txt", "utf-8").split("\n");
 
 function parseSeat(code) {
   var id = Belt_Array.reduce(code.split(""), 0, (function (id, letter) {
@@ -35,16 +38,56 @@ function parseSeat(code) {
         };
 }
 
-console.log(parseSeat("BFFFBBFRRR"));
+var seats = Belt_Array.map(lines, parseSeat);
 
-console.log(Belt_Array.reduce(Belt_Array.map(lines, parseSeat), 0, (function (max, param) {
-            return Math.max(max, param.id);
+var maxId = Belt_Array.reduce(seats, 0, (function (max, param) {
+        return Math.max(max, param.id);
+      }));
+
+console.log("Part A: " + String(maxId));
+
+var minId = Belt_Array.reduce(seats, maxId, (function (max, param) {
+        return Math.min(max, param.id);
+      }));
+
+var seatIdsSet = Belt_SetInt.fromArray(Belt_Array.map(seats, (function (param) {
+            return param.id;
           })));
 
+var possibleIds = Belt_Array.makeBy((maxId - minId | 0) + 1 | 0, (function (n) {
+        return n + minId | 0;
+      }));
+
+var emptySeats = Belt_Array.keep(possibleIds, (function (id) {
+        if (!Belt_SetInt.has(seatIdsSet, id) && Belt_SetInt.has(seatIdsSet, id - 1 | 0)) {
+          return Belt_SetInt.has(seatIdsSet, id + 1 | 0);
+        } else {
+          return false;
+        }
+      }));
+
+var id = Belt_Array.get(emptySeats, 0);
+
+if (id !== undefined) {
+  console.log("Part B: " + String(id));
+} else {
+  throw {
+        RE_EXN_ID: NoEmptySeat,
+        Error: new Error()
+      };
+}
+
 export {
-  lines ,
   InvalidLetter ,
+  NoEmptySeat ,
+  lines ,
   parseSeat ,
+  seats ,
+  maxId ,
+  minId ,
+  seatIdsSet ,
+  possibleIds ,
+  emptySeats ,
   
 }
 /* lines Not a pure module */
--- 2020/day5/src/Run.res
+++ 2020/day5/src/Run.res
@@ -1,8 +1,9 @@
 @module("fs") external readFileSync: (string, string) => string = "readFileSync"
 
-let lines: array<string> = readFileSync("input.txt", "utf-8")->Js.String2.split("\n")
-
 exception InvalidLetter(string)
+exception NoEmptySeat
+
+let lines: array<string> = readFileSync("input.txt", "utf-8")->Js.String2.split("\n")
 
 type seat = {
   id: int,
@@ -29,5 +30,25 @@ let parseSeat: string => seat = code => {
   }
 }
 
-Js.log(parseSeat("BFFFBBFRRR"))
-Js.log(lines->Array.map(parseSeat)->Array.reduce(0, (max, {id}) => Js.Math.max_int(max, id)))
+let seats = lines->Array.map(parseSeat)
+
+let maxId = seats->Array.reduce(0, (max, {id}) => Js.Math.max_int(max, id))
+
+Js.log(`Part A: ${string_of_int(maxId)}`)
+
+let minId = seats->Array.reduce(maxId, (max, {id}) => Js.Math.min_int(max, id))
+
+let seatIdsSet = Set.Int.fromArray(seats->Array.map(({id}) => id))
+
+let possibleIds = Array.makeBy(maxId - minId + 1, n => n + minId)
+
+let emptySeats =
+  possibleIds->Array.keep(id =>
+    !Set.Int.has(seatIdsSet, id) &&
+    Set.Int.has(seatIdsSet, id - 1) &&
+    Set.Int.has(seatIdsSet, id + 1)
+  )
+switch emptySeats[0] {
+| None => raise(NoEmptySeat)
+| Some(id) => Js.log(`Part B: ${string_of_int(id)}`)
+}
